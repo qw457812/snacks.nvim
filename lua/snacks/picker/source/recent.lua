595963140e464e9bd8244b758a590a7c0b5d0798
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-20 18:42:24 +0200

    fix(picker.recent): include closed / unlisted buffers in recent. Closes #1745

--- lua/snacks/picker/source/recent.lua
+++ lua/snacks/picker/source/recent.lua
@@ -33,7 +33,7 @@ function M.files(opts, ctx)
   local current_file = svim.fs.normalize(vim.api.nvim_buf_get_name(0), { _fast = true })
   ---@type number[]
   local bufs = vim.tbl_filter(function(b)
-    return vim.api.nvim_buf_get_name(b) ~= "" and vim.bo[b].buftype == "" and vim.bo[b].buflisted
+    return vim.api.nvim_buf_get_name(b) ~= "" and vim.bo[b].buftype == ""
   end, vim.api.nvim_list_bufs())
   table.sort(bufs, function(a, b)
     return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
